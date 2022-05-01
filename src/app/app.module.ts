34964a45dc95b64cd54839f1ff45e5eeb5bb0b0d
marcel-noehre <marcelnoehre@web.de>
2022-05-02 01:43:41 +0200

    raw starting-page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { StartingPageComponent } from './components/starting-page/starting-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideNavComponent
+    SideNavComponent,
+    StartingPageComponent
   ],
   imports: [
     BrowserModule,
