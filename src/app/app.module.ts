013884a4432a9a082b9ec998907e637e156663ba
marcel-noehre <marcelnoehre@web.de>
2022-05-02 18:34:59 +0200

    raw settings component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { StartingPageComponent } from './components/starting-page/starting-page.component';
 import { TypingAnimationComponent } from './components/typing-animation/typing-animation.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideNavComponent,
     StartingPageComponent,
-    TypingAnimationComponent
+    TypingAnimationComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
