7287e5203b95686e6f56da92fd8652f928f9d092
marcel-noehre <marcelnoehre@web.de>
2022-05-02 16:05:39 +0200

    raw typing-animation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { StartingPageComponent } from './components/starting-page/starting-page.component';
+import { TypingAnimationComponent } from './components/typing-animation/typing-animation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideNavComponent,
-    StartingPageComponent
+    StartingPageComponent,
+    TypingAnimationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/typing-animation/typing-animation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-typing-animation',
+  templateUrl: './typing-animation.component.html',
+  styleUrls: ['./typing-animation.component.scss']
+})
+export class TypingAnimationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
