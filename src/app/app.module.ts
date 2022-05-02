9926114afbc4fea832e8a4d504c881614f9a7b0d
marcel-noehre <marcelnoehre@web.de>
2022-05-02 17:51:01 +0200

    add raw tic-tac-toe component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { StartingPageComponent } from './components/starting-page/starting-page.component';
 import { TypingAnimationComponent } from './components/typing-animation/typing-animation.component';
+import { TicTacToeComponent } from './components/tic-tac-toe/tic-tac-toe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideNavComponent,
     StartingPageComponent,
-    TypingAnimationComponent
+    TypingAnimationComponent,
+    TicTacToeComponent
   ],
   imports: [
     BrowserModule,
