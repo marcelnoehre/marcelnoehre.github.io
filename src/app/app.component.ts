caa940c89b25f331abaf4f1d7896744860c6a59c
marcel-noehre <marcelnoehre@web.de>
2022-05-03 12:46:28 +0200

    language selector fallback

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { StorageService } from './services/storage.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'portfolio';
+
+  constructor(
+    private translate: TranslateService, 
+    private storage: StorageService
+    ) {
+    translate.setDefaultLang('de');
+    let lang = null;
+    try {
+      lang = storage.getSessionEntry('lang');
+    } catch(err) {}  
+    lang = lang? lang : 'de';
+    storage.setSessionEntry('lang', lang);
+    translate.use(lang);
+  }
 }
---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class StorageService {
+
+    public setSessionEntry(key: string, id: string): void {
+        sessionStorage.setItem(key, id);
+    }
+
+	public getSessionEntry(key: string): any {
+		return sessionStorage.getItem(key);
+	}
+}
