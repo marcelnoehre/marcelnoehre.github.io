440c2eae03d9e37e9522a0b540cc5b5263106bfd
marcel-noehre <marcelnoehre@web.de>
2022-05-03 12:37:21 +0200

    ngx/translate dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
@@ -8,6 +7,13 @@ import { StartingPageComponent } from './components/starting-page/starting-page.
 import { TypingAnimationComponent } from './components/typing-animation/typing-animation.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { BounceAnimationComponent } from './components/bounce-animation/bounce-animation.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -20,7 +26,16 @@ import { BounceAnimationComponent } from './components/bounce-animation/bounce-a
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+          }
+        })
   ],
   providers: [],
   bootstrap: [AppComponent]
