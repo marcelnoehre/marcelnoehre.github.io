09218577283da83f60f69620c8e49c677a38f2a1
marcel-noehre <marcelnoehre@web.de>
2022-05-06 10:35:52 +0200

    changed job identifier

--- src/app/services/typing-animation.service.ts
+++ src/app/services/typing-animation.service.ts
@@ -7,23 +7,23 @@ export class TypingAnimationService {
     private languages = new Map<string, Object>([
         ["en", {
 			name: 21,
-            job: 20
+            job: 25
 		}],
         ["de", {
 			name: 25,
-            job: 23
+            job: 28
 		}],
         ["es", {
 			name: 22,
-            job: 22
+            job: 30
 		}],
         ["fr", {
 			name: 24,
-            job: 23
+            job: 36
 		}],
         ["it", {
 			name: 24,
-            job: 22
+            job: 34
 		}]
     ]);
 
