9692d8d0e5b43016b1c18f221bac9f539488cdd6
marcel-noehre <marcelnoehre@web.de>
2022-10-21 00:29:12 +0200

    add 3d layer for title screen

--- src/app/components/core/starting-page/starting-page.component.ts
+++ src/app/components/core/starting-page/starting-page.component.ts
@@ -1,15 +1,30 @@
 import { Component, HostListener, OnInit } from '@angular/core';
+import { BreakpointService } from 'src/app/services/breakpoint.service';
 
 @Component({
   selector: 'app-starting-page',
   templateUrl: './starting-page.component.html',
   styleUrls: ['./starting-page.component.scss']
 })
-export class StartingPageComponent {
+export class StartingPageComponent implements OnInit {
   scrolled: boolean = false;
+  responsiveClass!: string;
 
   @HostListener('window:scroll', ['$event'])
   onWindowScroll() {
       this.scrolled = true;
   }
+
+  constructor(
+    private _breakpoint: BreakpointService
+  ) {
+
+  }
+
+  ngOnInit(): void {
+      this._breakpoint.responsiveClass$.subscribe((responsiveClass) => {
+        this.responsiveClass = responsiveClass;
+      });
+  }
+
 }
