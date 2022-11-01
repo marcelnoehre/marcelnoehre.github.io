39942e33d7e5b9365709c12dd416af3ed295f871
marcel-noehre <marcelnoehre@web.de>
2022-11-01 23:36:16 +0100

    raw projects overview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './components/core/homepage/homepage.component';
 import { CoreComponent } from './components/core/core/core.component';
+import { HomepageComponent } from './components/core/homepage/homepage.component';
+import { ProjectsOverviewComponent } from './components/shared/projects-overview/projects-overview.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CoreComponent,
     HomepageComponent,
-    CoreComponent
+    ProjectsOverviewComponent
   ],
   imports: [
     BrowserModule,
