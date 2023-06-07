a19372a1a3ced6da9874ee3c9c8bd101c349673d
marcel-noehre <marcelnoehre@web.de>
2023-06-08 00:00:30 +0200

    basic contact page layout

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,46 +6,9 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
-  contactForm!: FormGroup;
-
-  constructor() {
-    this.createForm();
-  }
-
-  private createForm(): void {
-		this.contactForm = new FormGroup(
-			{
-        nameFormControl: new FormControl('', { validators: [Validators.required] }),
-				emailFormControl: new FormControl('', { validators: [Validators.email] }),
-				subjectFormControl: new FormControl('', { validators: [Validators.required] }),
-        messageFormControl: new FormControl('', { validators: [Validators.required] })
-			}
-		);
-	}
-
-  get name(): string {
-    return this.contactForm.get('nameFormControl')!.value;
-  }
-
-  get email(): string {
-    return this.contactForm.get('emailFormControl')!.value;
-  }
-
-  get subject(): string {
-    return this.contactForm.get('subjectFormControl')!.value;
-  }
-
-  get message(): string {
-    return this.contactForm.get('messageFormControl')!.value;
-  }
-
-  sendMail() {
-    console.log('name: ', this.name);
-    console.log('email: ', this.email);
-    console.log('subject: ', this.subject);
-    console.log('message: ', this.message);
-    //TODO: send mail to me
-    //TODO: send automatic answer
-    //TODO: reset form
-  }
+  chatBotItems: string[] = [
+    "Question 1",
+    "Question 2",
+    "Question 3"
+  ];
 }
