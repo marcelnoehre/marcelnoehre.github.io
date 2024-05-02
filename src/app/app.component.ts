468038955397966606082a3cf79825cfd26e18b4
marcelnoehre <marcelnoehre@web.de>
2024-05-02 17:48:56 +0200

    replace project with clean v17 installation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,13 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { StorageService } from './services/storage.service';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  langs: string[] = ['en', 'de', 'es', 'fr'];
-  title: string = 'portfolio';
-
-  constructor(
-    private _storage: StorageService,
-    private _translate: TranslateService
-  ) {
-    let defaultLang: string = this.langs.includes(_translate.getBrowserLang() || '') ? _translate.getBrowserLang()! : 'en';
-    _translate.setDefaultLang(defaultLang)
-    _translate.use(_storage.getLocalEntry('lang') || defaultLang);
-  }
+  title = 'portfolio';
 }
