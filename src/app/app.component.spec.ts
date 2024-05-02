cb2c16a2ab123dac7898d15c61d204a20bd0a309
marcelnoehre <marcelnoehre@web.de>
2024-05-02 17:57:03 +0200

    cleanup angular standard template

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'portfolio' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('portfolio');
+    expect(app.title).toEqual('Marcel Nöhre');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, portfolio');
-  });
 });
