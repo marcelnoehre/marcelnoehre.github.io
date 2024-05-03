25f555e2edca827f162e198ad7a141ea2d399792
marcelnoehre <marcelnoehre@web.de>
2024-05-03 15:29:16 +0200

    basic 3d cube

--- src/app/components/core/core.component.ts
+++ src/app/components/core/core.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ScrollDirective } from '../../directives/scroll.directive';
 import { LandingPageComponent } from '../landing-page/landing-page.component';
+import { CubeComponent } from '../cube/cube.component';
 
 @Component({
   selector: 'app-core',
   standalone: true,
-  imports: [LandingPageComponent, ScrollDirective],
+  imports: [LandingPageComponent, CubeComponent, ScrollDirective],
   providers: [ScrollDirective],
   templateUrl: './core.component.html',
   styleUrl: './core.component.scss'
