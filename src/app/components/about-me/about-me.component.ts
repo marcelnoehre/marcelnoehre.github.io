ed0bec2b8374571c7bca990da8cf25a4a57e8e37
marcelnoehre <marcelnoehre@web.de>
2024-05-05 19:25:17 +0200

    fill about-me card

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,16 +1,29 @@
 import { AfterViewInit, Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+
+interface CV {
+  date: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatDividerModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent implements AfterViewInit {
   @Input() public scrollY: number = 0;
   @Input() public screenHeight: number = 0;
+  public cv: CV[] = [
+    { date: '2019 - 2023', description: 'B.Sc. Applied Computer Science (University of Hildesheim)'},
+    { date: 'Since 2023', description: 'M.Sc. Applied Computer Science (University of Hildesheim)'},
+    { date: '2021 (3 month)', description: 'Internship (Youco GmbH)' },
+    { date: '2022 & 2023', description: 'Teaching Assistant - Java (University of Hildesheim)' },
+    { date: 'Since 2021', description: 'Working Student (Entiac GmbH)' }
+  ];
 
   ngAfterViewInit() {
     try { 
