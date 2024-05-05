6acd6d1cf2c5c7e4c88396fa51febb2ab216c525
marcelnoehre <marcelnoehre@web.de>
2024-05-05 16:27:05 +0200

    animate about me card

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
@@ -8,6 +8,26 @@ import { MatCardModule } from '@angular/material/card';
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
-export class AboutMeComponent {
+export class AboutMeComponent implements AfterViewInit {
+  @Input() public scrollY: number = 0;
+  @Input() public screenHeight: number = 0;
 
+  ngAfterViewInit() {
+    try { 
+      this.scrollY = window.scrollY
+      this.screenHeight = window.innerHeight;
+    } catch (err) { }
+  }
+
+  cardDegree(): string {
+    if (this.scrollY <= (this.screenHeight * 0.7)) return '90deg';
+    if (this.scrollY >= (this.screenHeight * 1.2)) return '0deg';
+    return (90 * (this.screenHeight * 1.2 - this.scrollY) / (this.screenHeight * 1.2 - this.screenHeight * 0.7)) + 'deg';
+  }
+
+  cardFade(): number {
+    if (this.scrollY <= (this.screenHeight * 0.7)) return 0;
+    if (this.scrollY >= this.screenHeight) return 1;
+    return 1 - (this.screenHeight - this.scrollY) / (this.screenHeight - this.screenHeight * 0.7);
+  }
 }
