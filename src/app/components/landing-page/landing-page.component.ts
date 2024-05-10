9c8b15ec0c998ea8c023441d4e36a2580945be4d
marcelnoehre <marcelnoehre@web.de>
2024-05-10 15:10:29 +0200

    scroll down information

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,17 +1,33 @@
-import { AfterViewInit, Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import lottie from 'lottie-web';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, NgClass],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent implements AfterViewInit {
+  @ViewChild('scrollDown', { static: true }) animationContainer!: ElementRef;
+  @Input() public scrollY: number = 0;
   @Input() public screenWidth: number = 0;
   @Input() public screenHeight: number = 0;
+
+  ngOnInit(): void {
+    try {
+      lottie.loadAnimation({
+        container: this.animationContainer.nativeElement,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'assets/images/scroll-down.json'
+      });
+    } catch (err) { }
+  }
 
   ngAfterViewInit() {
     try { 
