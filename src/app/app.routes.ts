d381c403339d1d539f1f080a33bcd83ba64a4270
marcelnoehre <marcelnoehre@web.de>
2024-05-29 21:12:54 +0200

    impressum

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { CoreComponent } from './components/core/core.component';
 import { ComingSoonComponent } from './components/coming-soon/coming-soon.component';
+import { ImpressumComponent } from './pages/impressum/impressum.component';
 
 export const routes: Routes = [
     { path: '', component: CoreComponent },
+    { path: 'impressum', component: ImpressumComponent },
     { path: 'coming-soon', component: ComingSoonComponent },
     { path: '**', redirectTo: '' }
 ];
