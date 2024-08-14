93d5a5eea65c8bebf11fe1f7d5e7ceaca81eccec
marcelnoehre <marcelnoehre@web.de>
2024-08-14 17:56:21 +0200

    refactor folder structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { CoreComponent } from './components/core/core.component';
-import { ComingSoonComponent } from './components/coming-soon/coming-soon.component';
-import { ImpressumComponent } from './components/impressum/impressum.component';
+import { CoreComponent } from './pages/core/core.component';
+import { ComingSoonComponent } from './pages/coming-soon/coming-soon.component';
+import { ImpressumComponent } from './pages/impressum/impressum.component';
 
 export const routes: Routes = [
     { path: '', component: CoreComponent },
--- src/app/pages/core/core.component.ts
+++ src/app/pages/core/core.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { ScreenDirective } from '../../directives/screen.directive';
-import { LandingPageComponent } from '../landing-page/landing-page.component';
-import { CubeComponent } from '../cube/cube.component';
-import { AboutMeComponent } from '../about-me/about-me.component';
-import { ProjectsComponent } from '../projects/projects.component';
-import { GridComponent } from '../grid/grid.component';
-import { ContactComponent } from '../contact/contact.component';
-import { ScrollbarComponent } from '../scrollbar/scrollbar.component';
+import { LandingPageComponent } from '../../components/landing-page/landing-page.component';
+import { CubeComponent } from '../../components/cube/cube.component';
+import { AboutMeComponent } from '../../components/about-me/about-me.component';
+import { ProjectsComponent } from '../../components/projects/projects.component';
+import { GridComponent } from '../../components/grid/grid.component';
+import { ContactComponent } from '../../components/contact/contact.component';
+import { ScrollbarComponent } from '../../components/scrollbar/scrollbar.component';
 
 @Component({
   selector: 'app-core',
