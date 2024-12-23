b0dc7488ff59d7111eb8250e4dc45938278d5166
marcelnoehre <marcelnoehre@web.de>
2024-12-23 22:23:27 +0100

    integrate toolbar in project wallpaper

--- src/app/components/shared/project-wallpaper/project-wallpaper.component.ts
+++ src/app/components/shared/project-wallpaper/project-wallpaper.component.ts
@@ -1,12 +1,17 @@
 import { Component, Input } from '@angular/core';
+import { ScreenDirective } from '../../../directives/screen.directive';
+import { ToolbarComponent } from '../toolbar/toolbar.component';
 
 @Component({
   selector: 'app-project-wallpaper',
   standalone: true,
-  imports: [],
+  imports: [ToolbarComponent, ScreenDirective],
+  providers: [ScreenDirective],
   templateUrl: './project-wallpaper.component.html',
   styleUrl: './project-wallpaper.component.scss'
 })
 export class ProjectWallpaperComponent {
   @Input() public wallpaper!: string;
+  public screenWidth: number = 0;
+  public screenHeight: number = 0;
 }
--- src/app/pages/projects/nyc-taxi-analysis/nyc-taxi-analysis.component.ts
+++ src/app/pages/projects/nyc-taxi-analysis/nyc-taxi-analysis.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { ProjectWallpaperComponent } from '../../../components/shared/project-wallpaper/project-wallpaper.component';
 import { MatCardModule } from '@angular/material/card';
-import { ToolbarComponent } from '../../../components/shared/toolbar/toolbar.component';
 
 @Component({
   selector: 'app-nyc-taxi-analysis',
   standalone: true,
-  imports: [ToolbarComponent, ProjectWallpaperComponent, MatCardModule],
+  imports: [ProjectWallpaperComponent, MatCardModule],
   templateUrl: './nyc-taxi-analysis.component.html',
   styleUrl: './nyc-taxi-analysis.component.scss'
 })
 export class NycTaxiAnalysisComponent {
-  public wallpaper: string = 'assets/images/project/nyc-taxi-analysis-wallpaper.jpg';
+  public wallpaper: string = 'assets/images/projects/nyc-taxi-analysis/nyc-taxi-analysis-wallpaper.jpg';
 }
