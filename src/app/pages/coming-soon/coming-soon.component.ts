c381d039bdfd93cf159e90d51cbf5b681b029b91
marcelnoehre <marcelnoehre@web.de>
2025-01-02 17:18:39 +0100

    add returning option for coming-soon page

--- src/app/pages/coming-soon/coming-soon.component.ts
+++ src/app/pages/coming-soon/coming-soon.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-coming-soon',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './coming-soon.component.html',
   styleUrl: './coming-soon.component.scss'
 })
 export class ComingSoonComponent {
+  constructor(
+    private location: Location
+  ) {
+  }
 
+  public close(): void {
+    this.location.back();
+  }
 }
