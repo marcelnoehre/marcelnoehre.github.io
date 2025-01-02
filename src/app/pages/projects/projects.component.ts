b21b5ccb3a2be44976056b748ea2e67370b9e2f2
marcelnoehre <marcelnoehre@web.de>
2025-01-02 14:53:53 +0100

    projects page layout

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { ScreenDirective } from '../../directives/screen.directive';
+import { ToolbarComponent } from '../../components/shared/toolbar/toolbar.component';
+import { ContactComponent } from '../../components/core/contact/contact.component';
+import { ScrollbarComponent } from '../../components/shared/scrollbar/scrollbar.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [ToolbarComponent, ContactComponent, ScrollbarComponent, ScreenDirective],
+  providers: [ScreenDirective],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  public screenWidth: number = 0;
+  public screenHeight: number = 0;
+  public scrollY: number = 0;
 
 }
