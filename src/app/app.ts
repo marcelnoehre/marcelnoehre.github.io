c250eb228787268200950abb54d4dd0a076eeed4
marcelnoehre <marcelnoehre@web.de>
2025-09-17 15:41:36 +0200

    remove blobity

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
 import { AfterViewInit, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Theme } from './services/theme';
-import Blobity from 'blobity';
 
 @Component({
   selector: 'app-root',
@@ -9,25 +8,9 @@ import Blobity from 'blobity';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App implements AfterViewInit {
+export class App {
   protected readonly title = signal('Marcel Nöhre');
 
   constructor(public _: Theme) {}
 
-  ngAfterViewInit(): void {
-    try { 
-      new Blobity({
-        licenseKey: '...',
-        dotColor: "#7b68ee",
-        focusableElementsOffsetX: 5,
-        focusableElementsOffsetY: 5,
-        color: "#7b68ee",
-        opacity: 0.25,
-        magnetic: true,
-        radius: 5,
-        zIndex: 1000,
-        dotSize: 10
-      });
-    } catch (err) { }
-  }
 }
