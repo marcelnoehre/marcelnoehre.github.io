d203a86e1981ff2f62050e55cf5fd61764db5e63
marcelnoehre <marcelnoehre@web.de>
2025-09-17 15:57:24 +0200

    landing page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Navigation } from '../../components/navigation/navigation';
+import { Screen } from '../../services/screen';
+import { Cube } from '../../components/cube/cube';
+import { Scrollbar } from '../../components/scrollbar/scrollbar';
+import { CommonModule } from '@angular/common';
+import lottie from 'lottie-web';
 
 @Component({
   selector: 'app-home',
-  imports: [Navigation],
+  imports: [CommonModule, Navigation, Cube, Scrollbar],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
+  @ViewChild('scrollDown', { static: true }) animationContainer!: ElementRef;
+  protected width: number = 0;
+  protected height: number = 0;
+  protected scrollY: number = 0;
+  protected firstName: Array<string> = ['M', 'a', 'r', 'c', 'e', 'l'];
+  protected lastName: Array<string> = ['N', 'ö', 'h', 'r', 'e'];
 
+  constructor(private _screen: Screen) { }
+
+  ngOnInit() {
+    this._screen.width$.subscribe(w => this.width = w);
+    this._screen.height$.subscribe(h => this.height = h);
+    this._screen.scrollY$.subscribe(y => this.scrollY = y);
+    try {
+      lottie.loadAnimation({
+        container: this.animationContainer.nativeElement,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'assets/images/icons/scroll-down.json'
+      });
+    } catch (err) { }
+  }
 }
