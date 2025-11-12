fc61596af5d1103b66155ff7bb831e648dd1d41f
marcelnoehre <marcelnoehre@web.de>
2025-11-12 10:41:16 +0100

    basic cv page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Cv } from './pages/cv/cv';
 
 export const routes: Routes = [
     { path: '', component: Home },
+    { path: 'cv', component: Cv }
 ];
---
+++ src/app/pages/cv/cv.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Data } from '../../services/data';
+import { ExperienceItem } from '../../interfaces/experience-item';
+import { EducationItem } from '../../interfaces/education-item';
+import { MatIconModule } from '@angular/material/icon';
+import { Navigation } from '../../components/navigation/navigation';
+
+@Component({
+  selector: 'app-cv',
+  imports: [MatIconModule, Navigation],
+  templateUrl: './cv.html',
+  styleUrl: './cv.scss'
+})
+export class Cv implements OnInit {
+  protected cv: string[] = ['C', 'u', 'r', 'r', 'i', 'c', 'u', 'l', 'u', 'm', '&nbsp', 'V', 'i', 't', 'a', 'e'];
+  protected experience: ExperienceItem[] = [];
+  protected education: EducationItem[] = [];
+
+  constructor(private _data: Data) {}
+
+  ngOnInit() {
+    this.experience = this._data.experience;
+    this.education = this._data.education;
+  }
+}
