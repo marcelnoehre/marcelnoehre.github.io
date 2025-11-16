4ac26561727766bd06d4f6dc0e05b738ab6a95b8
marcelnoehre <marcelnoehre@web.de>
2025-11-16 15:03:48 +0100

    projects page

--- src/app/pages/projects/projects.ts
+++ src/app/pages/projects/projects.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Navigation } from '../../components/navigation/navigation';
 import { Contact } from '../../components/contact/contact';
+import { Data } from '../../services/data';
+import { ProjectItem } from '../../interfaces/project-item';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-projects',
-  imports: [Navigation, Contact],
+  imports: [Navigation, Contact, MatIconModule],
   templateUrl: './projects.html',
   styleUrl: './projects.scss'
 })
-export class Projects {
+export class Projects implements OnInit {
   protected title: string[] = ['P', 'r', 'o', 'j', 'e', 'c', 't', 's'];
+  protected projects: ProjectItem[] = [];
+
+  constructor(private _data: Data) { }
+
+  ngOnInit() {
+    this.projects = this._data.projects;
+  }
+
 }
