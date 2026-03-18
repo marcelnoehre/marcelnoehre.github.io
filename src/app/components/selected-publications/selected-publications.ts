5dc2e77f58f51b1fefea96ef62b470682479d0de
marcelnoehre <marcelnoehre@web.de>
2026-03-18 11:31:57 +0100

    selected publications

--- src/app/components/selected-publications/selected-publications.ts
+++ src/app/components/selected-publications/selected-publications.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { PublicationItem } from '../../interfaces/publications-item';
+import { Data } from '../../services/data';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-selected-publications',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './selected-publications.html',
   styleUrl: './selected-publications.scss'
 })
 export class SelectedPublications {
+  protected publications: PublicationItem[] = [];
 
+  constructor(
+    private _router: Router,
+    private _data: Data
+  ) {}
+  
+  ngOnInit(): void {
+    this.publications = this._data.publications
+  }
+
+  redirect(): void {
+    this._router.navigate(['/publications']).then(() => {
+      window.scrollTo(0, 0)
+    });
+  }
 }
